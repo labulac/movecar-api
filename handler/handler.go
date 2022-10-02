a1be8546d4baf1fe5ff3d5910a6a794a501d73e5
labulac <740162752@qq.com>
2022-10-02 21:28:43 +0800

    1

---
+++ handler/handler.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/tidwall/gjson"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Ping(c *gin.Context) {
+	c.String(http.StatusOK, "pong")
+}
+
+func ErrRouter(c *gin.Context) {
+	c.String(http.StatusBadRequest, "url err")
+}
+
+func Gettel(c *gin.Context) {
+
+	test := resty.New().SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	resp, err := test.R().Get("http://home.labulac.top:40080/gettel")
+	if err != nil {
+
+		c.String(http.StatusBadRequest, "err1")
+	}
+	tel := gjson.Get(resp.String(), "msg")
+
+	c.String(http.StatusOK, tel.String())
+}
+
+func Changetel(c *gin.Context) {
+	tel := c.Param("tel")
+	test := resty.New().SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	resp, err := test.R().Get("http://home.labulac.top:40080/changetel/" + ":" + tel)
+	if err != nil {
+
+		c.String(http.StatusBadRequest, "err2")
+	}
+	telphote := gjson.Get(resp.String(), "msg")
+
+	c.String(http.StatusOK, telphote.String())
+
+}
